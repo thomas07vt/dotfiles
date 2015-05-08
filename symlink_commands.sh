0a72a833844efb3010e8855b2944902e2c5da8ad
John Thomas <thomas07@vt.edu>
2015-05-08 08:54:30 -0700

    Adding pathogen and NERDTree

--- symlink_commands.sh
+++ symlink_commands.sh
@@ -1,6 +1,16 @@
+cur_dir=`pwd`
+
 # Symbolic link for .vimrc
-ln -s "`pwd`/.vimrc" ~/.vimrc
+ln -s "$cur_dir/.vimrc" ~/.vimrc
 
 # Set up the  molokai vim color scheme 
 mkdir -p ~/.vim/colors
-ln -s "`pwd`/colors/monokai.vim" ~/.vim/colors/monokai.vim
+ln -s "$cur_dir/colors/monokai.vim" ~/.vim/colors/monokai.vim
+
+# Install pathogen.vim for plugins and runtime files
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+ln -s "$cur_dir/autoload/pathogen.vim" ~/.vim/autoload/pathogen.vim
+
+# Install NERDTree
+ln -s "$cur_dir/bundle/nerdtree" ~/.vim/bundle/nerdtree
+
